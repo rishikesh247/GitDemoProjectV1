9029b53bcf3ebc347926ec0932b13414eb20c6e1
in232rischa <in232rischa@CSPLAP0905.ext.crifnet.com>
2020-06-18 17:37:00 +0530

    commiting TestExample2

---
+++ src/com/test/TestExample2.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class TestExample2 {
+
+	
+	public static void main(String[] args) {
+		System.out.println("My First program for git repository as a engineer....");
+	}
+	
+}
